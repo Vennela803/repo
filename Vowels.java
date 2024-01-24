e43941af2d971cad239d131d90c6fbb59966a2df
vennela0803 <vennelagodugu@gmail.com>
2024-01-24 13:08:07 +0530

    first commit

---
+++ Vowels.java
@@ -0,0 +1,31 @@
+class  Vowels          
+{
+ public static void main(String[]args)
+{
+   char ch ='O';
+     if(ch>='A'&& ch<='Z')
+{
+    System.out.println("im capital");
+
+}
+
+        if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+{
+       
+         System.out.println(" im vowel");
+}
+         
+
+      else if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{     
+
+
+      System.out.println(" im vowel"); 
+}
+
+     else
+{
+           System.out.println(" im consonant");
+}
+}
+}
